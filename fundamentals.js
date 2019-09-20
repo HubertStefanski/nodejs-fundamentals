f140b12fbdca1a5eabd037f55a9ef614cd170952
HubertStefanski <hubertstefanski@gmail.com>
2019-09-20 08:28:49 -0700

    Dynamic properties added

--- fundamentals.js
+++ fundamentals.js
@@ -5,5 +5,17 @@ let aCar ={
         cc: '1.8'},
     registration:'10WD1058'
 };
+//new Property
+aCar.mileage = '80000';
+aCar.color = {
+    exterior: 'red',
+    interior : {
+        texture: 'leather',
+        shade: 'cream',
+    },
+
+}
+
 
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log('it is a ' + aCar.color.exterior + ' car '+ aCar.mileage + ' mileage, and with ' + aCar.color.interior.texture + ' interior ');

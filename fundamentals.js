519e5f4855b4386166573751d2d03546d30ca2f5
HubertStefanski <hubertstefanski@gmail.com>
2019-09-20 09:01:18 -0700

    Loop Constructs

--- fundamentals.js
+++ fundamentals.js
@@ -16,6 +16,8 @@ let aCar = {
         model: 'Corrola',
         cc: '1.8'
     },
+    features: ['Parking Assist','Alarm','Tow-bar']
+    ,
     registration: { year: 10, county: 'WD', number: 1058 }
 };
 //new Property
@@ -33,3 +35,12 @@ aCar.color = {
 console.log(aCar.owner + ' drives a ' + aCar.type.make + ' ' + aCar.type.model);
 console.log('it is a ' + aCar.color.exterior + ' car ' + aCar.mileage + ' mileage, and with ' + aCar.color.interior.texture + ' interior ');
 console.log('First owner: ' + aCar.previous_owners[0].name + '-' + aCar.previous_owners[0].address)
+for(let i=0; i<aCar.features.length ;i++){
+    console.log(aCar.features[i]);
+}
+for(let i=0; i<aCar.previous_owners.length;i++){
+    console.log(aCar.previous_owners[i].name);
+}
+for(let p in aCar.type){
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p]);
+}

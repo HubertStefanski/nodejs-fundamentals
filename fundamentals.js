71b2ae98990354330b836c464e22ec681dc954d9
HubertStefanski <hubertstefanski@gmail.com>
2019-09-20 08:51:43 -0700

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,16 +1,28 @@
-let aCar ={
+let aCar = {
     owner: 'Joe bloggs',
-    type : {
+    address: '3 walkers lane',
+    previous_owners: [
+        {
+            name: 'Pat Smith ',
+            address: '1 Main Street'
+        },
+        {
+            name: 'Sheila Dwyer',
+            address: '2 High Street'
+        },
+    ],
+    type: {
         make: 'Toyota',
         model: 'Corrola',
-        cc: '1.8'},
-    registration:'10WD1058'
+        cc: '1.8'
+    },
+    registration: { year: 10, county: 'WD', number: 1058 }
 };
 //new Property
 aCar.mileage = '80000';
 aCar.color = {
     exterior: 'red',
-    interior : {
+    interior: {
         texture: 'leather',
         shade: 'cream',
     },
@@ -19,4 +31,5 @@ aCar.color = {
 
 
 console.log(aCar.owner + ' drives a ' + aCar.type.make + ' ' + aCar.type.model);
-console.log('it is a ' + aCar.color.exterior + ' car '+ aCar.mileage + ' mileage, and with ' + aCar.color.interior.texture + ' interior ');
+console.log('it is a ' + aCar.color.exterior + ' car ' + aCar.mileage + ' mileage, and with ' + aCar.color.interior.texture + ' interior ');
+console.log('First owner: ' + aCar.previous_owners[0].name + '-' + aCar.previous_owners[0].address)
